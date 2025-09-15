815df22dd07fe81be26067d5fc1c6da19aba6d2d
Connor Kasper <clkasper@stu.naperville203.org>
2025-09-15 13:07:27 -0500

    IM STACKING IT mini pekka: PANCAKES

--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -9,5 +9,26 @@ public class StackDemo
 {
     public static void main(String[] args)
     {
+        Stack<String> commands = new Stack<>();
+
+        // Push a lot of commands on top of the stack
+        commands.push("Insert: 'Hello'");
+        commands.push("Insert: ','");
+        commands.push("Insert: ' ");
+        commands.push("Insert: 'World'");
+        commands.push("Insert: '?'");
+        commands.push("Delete: '?'");
+        commands.push("Insert: '!'");
+
+        // print the stack
+        System.out.println("Current stack: " + commands);
+
+        // User presses undo button 4 times
+        for(int i = 0; i < 4; i++)
+        {
+            String command = commands.pop();
+            System.out.println("Undoing command: " + command);
+        }
+        System.out.println("Current stack: " + commands);
     }
 }
